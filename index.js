84fe15fe600ae18dbf64f04b11df3e65453032d1
bosmat-haifa <drdy.business@gmail.com>
2020-09-25 18:39:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const { Client } = require('pg');
+const {
+    Client
+} = require('pg');
 const express = require("express");
 const app = express();
 const path = require('path');
 const http = require("http");
-const { wakeDyno } = require('heroku-keep-awake');
+const {
+    wakeDyno
+} = require('heroku-keep-awake');
 
 
-const client = new Client({
+const db = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
+        rejectUnauthorized: false
     }
-  });
+});
 
-client.connect();
+db.connect();
 
-client.query("INSERT INTO users (id, password, email, name) VALUES (1234567890, '69420666', 'roiesholet@gmail.com', 'Roie')", (err, res) => {
-    if (err) throw err;
-    client.end();
-  });
+db.query("INSERT INTO users (id, password, email, name) VALUES (1234567890, '69420666', 'roiesholet@gmail.com', 'Roie')", (err, res) => {
+    if (err) {
+        db.end();
+        throw err;
+    }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -26,17 +32,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'Login_v2/index.html')));
 app.get('/db', (req, res) => {
-    client.query("SELECT * FROM users", (err, res_) => {
-        if (err) throw err;
+    db.query("SELECT * FROM users", (err, res_) => {
+        if (err) {
+            db.end();
+            throw err;
+        }
         res.send(`<h1>${res_.rows.join('\n')}</h1>`);
-        client.end();
     });
 });
 
 
 const PORT = process.env.PORT || 8080;
 const DYNO_URL = 'https://lucidlearn.herokuapp.com';
-http.createServer(app).listen(PORT, function() {
+http.createServer(app).listen(PORT, function () {
     wakeDyno(DYNO_URL);
     console.log('Express server listening on port ' + PORT);
 });
