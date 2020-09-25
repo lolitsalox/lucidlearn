d1185592e4f7e9e9339adc135e4267d97f07e720
bosmat-haifa <drdy.business@gmail.com>
2020-09-25 18:12:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,6 @@ client.connect();
 //     }
 //     client.end();
 //   });
-client.query("CREATE TABLE IF NOT EXISTS test_table (uid int, name text)", (err, res) => {
-    if (err) throw err;
-    client.end();
-});
-count = 0;
-client.query("INSERT INTO test_table VALUES ($1, $2)", [count, "nisim maxim"], (err, res) => {
-    if (err) throw err;
-    client.end();
-});
 
 app.use(express.static(path.join(__dirname, "public")));
 
