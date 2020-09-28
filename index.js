9bb6c9383c8753e0db14ca8933770d60b95173ae
bosmat-haifa <drdy.business@gmail.com>
2020-09-28 17:36:17 +0300

    k

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function generateID() {
 
 async function userWith(field, value) {
     try {
-        const result = await db.query("SELECT $1 FROM users WHERE $1=$2", [field, value]);
+        const result = await db.query("SELECT id FROM users WHERE $1=$2", [field, value]);
         console.log(result.rows, result.rowCount);
         return result.rowCount > 0;
     } catch (err) {
