d0c200f2004a7c4707aadb7ac1ace36c0dc7a642
bosmat-haifa <drdy.business@gmail.com>
2020-09-28 17:18:02 +0300

    shinei

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ async function generateID() {
 
 async function userWith(field, value) {
     try {
-        const result = await db.query("SELECT $1 FROM users WHERE $1 = $2", [field, value]);
+        const result = await db.query("SELECT $1 FROM users WHERE $1=$2", [field, value]);
+        console.log(result.rows);
         return result.rowCount > 0;
     } catch (err) {
         console.log(err);
